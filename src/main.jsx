06104bab9fc447519b5a8054070e68e9dc020771
VictorCDS-p <Victor90774@outlook.com>
2024-10-01 19:49:21 -0300

    BloodDonationForm components fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import BloodDonationForm from './components'
+import BloodDonationForm from './components/BloodDonationForm'
 
 
 createRoot(document.getElementById('root')).render(
