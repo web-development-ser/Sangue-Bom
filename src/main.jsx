ebabd661a2b087f7342dcc97ecf4b152c52732e6
unknown <raissarlsantos@gmail.com>
2024-10-01 20:07:57 -0300

    criei o header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import BloodDonationForm from './components/BloodDonationForm'
+import Footer from './components/footer/index'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BloodDonationForm />
+    <Footer/>
+
   </StrictMode>,
 )
