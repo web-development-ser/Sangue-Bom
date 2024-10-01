8eb4b34d8205b737979d22703c31bb3136b3aeb2
Ant Sérgio <sergio-web-development@protonmail.com>
2024-10-01 19:56:55 -0300

    Update.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import BloodDonationForm from './components'
+import { Hello } from './teste'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BloodDonationForm />
+    <Hello />
   </StrictMode>,
 )
