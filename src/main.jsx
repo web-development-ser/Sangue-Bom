fade9808f18810ba4ad9c9ab012ad75d76bbcbf9
VictorCDS-p <Victor90774@outlook.com>
2024-10-01 20:24:26 -0300

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import BloodDonationForm from './components'
-
 import BloodDonationForm from './components/BloodDonationForm'
 import Footer from './components/footer/index'
-import { Hello } from './teste'
 
 
 
@@ -14,7 +10,5 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BloodDonationForm />
     <Footer/>
-
-    <Hello />
   </StrictMode>,
 )
