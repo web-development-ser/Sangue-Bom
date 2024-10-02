981b6144d5b9c7eab0ba42e87841eea3f575abba
VictorCDS-p <Victor90774@outlook.com>
2024-10-02 13:13:55 -0300

    BloodDonorList

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import BloodDonationForm from './components/BloodDonationForm'
+import BloodDonorList from './components/BloodDonorList'
+
 import Footer from './components/footer/index'
 
 
@@ -9,6 +11,7 @@ import Footer from './components/footer/index'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BloodDonationForm />
+    <BloodDonorList />
     <Footer/>
   </StrictMode>,
 )
